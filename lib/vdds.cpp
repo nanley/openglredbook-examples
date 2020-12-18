312967c682fdf65f36f7cb2ad69a31a705791896
Nanley Chery <nanleychery@gmail.com>
2020-12-17 17:42:41 -0800

    Avoid gotos in vdds.cpp
    Avoids errors like the following:
    
    [ 11%] Building CXX object CMakeFiles/vermilion.dir/lib/vdds.cpp.o
    /home/nchery/repos/openglredbook-examples/lib/vdds.cpp: In function ‘void vglLoadDDS(const char*, vglImageData*)’:
    /home/nchery/repos/openglredbook-examples/lib/vdds.cpp:650:6: error: jump to label ‘done_close_file’
      650 |      done_close_file:
          |      ^~~~~~~~~~~~~~~
    /home/nchery/repos/openglredbook-examples/lib/vdds.cpp:590:11: note:   from here
      590 |      goto done_close_file;
          |           ^~~~~~~~~~~~~~~
    /home/nchery/repos/openglredbook-examples/lib/vdds.cpp:628:9: note:   crosses initialization of ‘int depth’
      628 |     int depth = file_header.std_header.depth;
          |         ^~~~~
    /home/nchery/repos/openglredbook-examples/lib/vdds.cpp:627:9: note:   crosses initialization of ‘int height’
      627 |     int height = file_header.std_header.height;
          |         ^~~~~~
    /home/nchery/repos/openglredbook-examples/lib/vdds.cpp:626:9: note:   crosses initialization of ‘int width’
      626 |     int width = file_header.std_header.width;
          |         ^~~~~
    /home/nchery/repos/openglredbook-examples/lib/vdds.cpp:624:15: note:   crosses initialization of ‘GLubyte* ptr’
      624 |     GLubyte * ptr = reinterpret_cast<GLubyte*>(image->mip[0].data);
          |               ^~~
    /home/nchery/repos/openglredbook-examples/lib/vdds.cpp:612:12: note:   crosses initialization of ‘size_t current_pos’
      612 |     size_t current_pos = ftell(f);
          |            ^~~~~~~~~~~
    make[2]: *** [CMakeFiles/vermilion.dir/build.make:121: CMakeFiles/vermilion.dir/lib/vdds.cpp.o] Error 1
    make[1]: *** [CMakeFiles/Makefile2:1122: CMakeFiles/vermilion.dir/all] Error 2
    make: *** [Makefile:149: all] Error 2

--- lib/vdds.cpp
+++ lib/vdds.cpp
@@ -587,7 +587,8 @@ void vglLoadDDS(const char* filename, vglImageData* image)
 
     if (file_header.magic != DDS_MAGIC)
     {
-        goto done_close_file;
+        fclose(f);
+        return;
     }
 
     if (file_header.std_header.ddspf.dwFourCC == DDS_FOURCC_DX10)
@@ -596,12 +597,18 @@ void vglLoadDDS(const char* filename, vglImageData* image)
     }
 
     if (!vgl_DDSHeaderToImageDataHeader(file_header, image))
-        goto done_close_file;
+    {
+        fclose(f);
+        return;
+    }
 
     image->target = vgl_GetTargetFromDDSHeader(file_header);
 
     if (image->target == GL_NONE)
-        goto done_close_file;
+    {
+        fclose(f);
+        return;
+    }
 
     size_t current_pos = ftell(f);
     size_t file_size;
@@ -641,9 +648,6 @@ void vglLoadDDS(const char* filename, vglImageData* image)
         height >>= 1;
         depth >>= 1;
     }
-
-done_close_file:
-    fclose(f);
 }
 
 }
